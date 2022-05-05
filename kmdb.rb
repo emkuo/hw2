17fd54965e4f513b92cf39507226365704e6ed33
emkuo <emily.kuo@kellogg.northwestern.edu>
2022-05-05 01:24:38 +0000

    edits_1

--- kmdb.rb
+++ kmdb.rb
@@ -68,15 +68,181 @@
 # The Dark Knight Rises  Anne Hathaway         Selina Kyle
 
 # Delete existing data, so you'll start fresh each time this script is run.
-# Use `Model.destroy_all` code.
-# TODO!
+Movie.destroy_all
+Studio.destroy_all
+Actor.destroy_all
+Role.destroy_all
 
 # Generate models and tables, according to the domain model.
-# TODO!
+# Currently in the migrate section the code exists there but copied and commented out below for reference 
 
+# class CreateMovies < ActiveRecord::Migration[7.0]
+#     def change
+#       create_table :movies do |t|
+#         t.string "title"
+#         t.integer "year_released"
+#         t.string "rated"
+#         t.integer "studio_id"
+#         t.timestamps
+#       end
+#     end
+#   end
+  
+# class CreateStudios < ActiveRecord::Migration[7.0]
+#     def change
+#       create_table :studios do |t|
+#         t.string "name"
+#         t.timestamps
+#       end
+#     end
+#   end
+
+# class CreateActors < ActiveRecord::Migration[7.0]
+#     def change
+#       create_table :actors do |t|
+#         t.string "name"
+#         t.timestamps
+#       end
+#     end
+#   end
+  
+# class CreateRoles < ActiveRecord::Migration[7.0]
+#     def change
+#       create_table :roles do |t|
+#         t.integer "movie_id"
+#         t.integer "actor_id"
+#         t.string "character_name"
+#         t.timestamps
+#       end
+#     end
+#   end
+  
 # Insert data into the database that reflects the sample data shown above.
 # Do not use hard-coded foreign key IDs.
-# TODO!
+
+# movies
+new_movie = Movie.new
+new_movie ["title"] = "Batman Begins"
+new_movie ["year_released"] = "2005"
+new_movie ["rated"] = "PG-13"
+new_movie ["studio_id"] = 1
+new_movie.save
+
+new_movie2 = Movie.new
+new_movie2 ["title"] = "The Dark Knight"
+new_movie2 ["year_released"] = "2008"
+new_movie2 ["rated"] = "PG-13"
+new_movie2 ["studio_id"] = 1
+new_movie2.save
+
+new_movie3 = Movie.new
+new_movie3 ["title"] = "The Dark Knight Rises"
+new_movie3 ["year_released"] = "2012"
+new_movie3 ["rated"] = "PG-13"
+new_movie3 ["studio_id"] = 1
+new_movie3.save
+
+# Studio 
+new_studio = Studio.new
+new_studio ["name"] = "Warner Bros"
+new_studio.save
+
+# Actors 
+new_actor = Actor.new
+new_actor["name"] = "Christian Bale"
+new_actor.save
+
+new_actor2 = Actor.new
+new_actor2["name"] = "Michael Caine"
+new_actor2.save
+
+new_actor3 = Actor.new
+new_actor3 ["name"] = "Liam Neeson"
+new_actor3.save
+
+new_actor4 = Actor.new
+new_actor4 ["name"] = "Katie Holmes"
+new_actor4.save
+
+new_actor5 = Actor.new
+new_actor5 ["name"] = "Gary Oldman"
+new_actor5.save
+
+new_actor6 = Actor.new
+new_actor6 ["name"] = "Heath Ledger"
+new_actor6.save
+
+new_actor7 = Actor.new
+new_actor7 ["name"] = "Aaron Eckhart"
+new_actor7.save
+
+new_actor8 = Actor.new
+new_actor8 ["name"] = "Maggie Gyllenhaal"
+new_actor8.save
+
+new_actor9 = Actor.new
+new_actor9 ["name"] = "Tom Hardy"
+new_actor9.save
+
+new_actor9 = Actor.new
+new_actor9 ["name"] = "Joseph Gordon-Levitt"
+new_actor9.save
+
+new_actor9 = Actor.new
+new_actor9 ["name"] = "Anne Hathaway"
+new_actor9.save
+
+# Characters 
+new_role = Role.new
+new_role ["actor_id"] = ""
+new_role ["character_name"] = "Bruce Wayne"
+new_role.save
+
+new_role2 = Role.new
+new_role2 ["actor_id"] = ""
+new_role2 ["character_name"] = "Alfred"
+new_role2.save
+
+new_role3 = Role.new
+new_role3 ["actor_id"] = ""
+new_role3 ["character_name"] = "Ra's Al Ghul"
+new_role3.save
+
+new_role4 = Role.new
+new_role4  ["actor_id"] = ""
+new_role4  ["character_name"] = "Rachel Dawes"
+new_role4.save
+
+new_role5 = Role.new
+new_role5 ["actor_id"] = ""
+new_role5 ["character_name"] = "Commissioner Gordon"
+new_role5.save
+
+new_role6 = Role.new
+new_role6 ["actor_id"] = ""
+new_role6 ["character_name"] = "Joker"
+new_role6.save
+
+new_role7 = Role.new
+new_role7 ["actor_id"] = ""
+new_role7 ["character_name"] = "Harvey Dent"
+new_role7.save
+
+new_role8 = Role.new
+new_role8 ["actor_id"] = ""
+new_role8 ["character_name"] = "Bane"
+new_role8.save
+
+new_role9 = Role.new
+new_role9 ["actor_id"] = ""
+new_role9 ["character_name"] = "John Blake"
+new_role9.save
+
+new_role10 = Role.new
+new_role10 ["actor_id"] = ""
+new_role10 ["character_name"] = "Selina Kyle"
+new_role10.save
+
 
 # Prints a header for the movies output
 puts "Movies"
